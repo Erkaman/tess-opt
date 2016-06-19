061929c35968b3914ebc636771950f8a9987bfe5
Eric Arnebäck <arnebackeric@gmail.com>
2016-06-19 08:58:52 +0200

    clean up.

--- src/simple.fs
+++ src/simple.fs
@@ -12,10 +12,6 @@ uniform int uDoVertexCalculation;
 
 void main()
 {
-//    vec3 diff = vec3( abs(fsNormal) );
-
-//    color = vec4(diff, 1.0);
-
     if(uDoVertexCalculation==1) {
 	color = fsResult;
 
